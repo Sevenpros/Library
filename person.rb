4ee41460e32062fc0dd3b001ca4e941ebe4d03b4
Sevenpros <sethbisoso@gmail.com>
2022-10-09 19:46:00 +0200

    Fix linter errors

--- person.rb
+++ person.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # this is the Person classs
 class Person
   def initialize(age, name = 'Unknown', parent_permission: true)
--- student.rb
+++ student.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 required './person'
 
 # this is the Student class
